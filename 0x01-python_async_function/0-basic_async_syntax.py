96b8b26fc23abda9cd0ed16e0ab971fd3a2d311f
DAVENJAGI <njagidave39@gmail.com>
2024-03-18 14:53:42 +0300

    Asynchronous_coroutine_Task_1

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import random
+
+
+async def wait_random(max_delay=10.0):
+    """
+    An async co-routine that awaits for random delay btwn 0 and max_delay
+    """
+    wait_time = random.uniform(0, max_delay)
+    await asyncio.sleep(wait_time)
+    return wait_time
+
+
+async def main():
+    delay = await wait_random(5.0)
+    print(f"waited for {delay:.2f} seconds")
+
+
+if __name__ == '__main__':
+    import asyncio
+    asyncio.run(main())
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,1 @@
+Folder containing the basics of asynchronous co-routines in python
