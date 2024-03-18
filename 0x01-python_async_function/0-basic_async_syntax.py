6397a347a7ebba6e962875a7768366ad9fd0f504
DAVENJAGI <njagidave39@gmail.com>
2024-03-18 15:05:25 +0300

    Asynchronous_coroutine_Task_1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import random
+import asyncio
 
 
-async def wait_random(max_delay=10.0):
+async def wait_random(max_delay=10.0) -> float:
     """
     An async co-routine that awaits for random delay btwn 0 and max_delay
     """
@@ -11,15 +12,3 @@ async def wait_random(max_delay=10.0):
     await asyncio.sleep(wait_time)
     return wait_time
 
-
-async def main():
-    """
-    Awaits for random time between 0 and default wait time
-    """
-    delay = await wait_random(5.0)
-    print(f"waited for {delay:.2f} seconds")
-
-
-if __name__ == '__main__':
-    import asyncio
-    asyncio.run(main())
