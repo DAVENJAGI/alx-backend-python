d7ed86dadb9dd35a157c9aec6ad6d626bc490311
DAVENJAGI <njagidave39@gmail.com>
2024-03-19 13:20:12 +0300

    Async_generators

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-""" Async comprenhesion gen """
+""" Async comprenhesion generator that takes no argument.
+The coroutine collects 10 random numbers using an async
+comprehensing over async_generator
+"""
 from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
