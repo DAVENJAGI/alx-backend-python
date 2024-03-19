77f836e67988aa9b3a7e94ec37fe3e34ec4da0c5
DAVENJAGI <njagidave39@gmail.com>
2024-03-19 12:37:30 +0300

    Async_generators

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,9 +7,11 @@ writes a co-routine ca;lled async_comprehension
 
 import asyncio
 from typing import List
-generated = __import__('0-async_generator').async_generator
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """collect ten rando numbers using async comprehension"""
-    return ([i async for i in generated()])
+    """
+    collect ten rando numbers using async comprehension
+    """
+    return ([i async for i in async_generator()])
