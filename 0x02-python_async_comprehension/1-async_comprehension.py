195260210c3e7e8d0bb0a4f17c047fa0c99e7159
DAVENJAGI <njagidave39@gmail.com>
2024-03-19 12:35:58 +0300

    Async_generators

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,6 +3,8 @@
 Imports async function from the previous task
 writes a co-routine ca;lled async_comprehension
 """
+
+
 import asyncio
 from typing import List
 generated = __import__('0-async_generator').async_generator
