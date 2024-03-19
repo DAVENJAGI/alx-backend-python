cb31bd88c5276232705aa90519738d46784f9876
DAVENJAGI <njagidave39@gmail.com>
2024-03-19 12:59:20 +0300

    Async_generators

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ Async comprenhesion gen """
-from typing import Generator
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """
     Generate numbera with async comprenhension
     """
